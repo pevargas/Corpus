14058af4fc7ec1416c005bc4a3c256f1ccbc542c
Patrick <pevargas90@gmail.com>
2014-01-30 21:27:43 -0700

    Got the parallax working
    As well as the navbar scroll to position. Also began working on styling
    of sidebar. Need to really make this code my own.

--- flat/flat.js
+++ flat/flat.js
@@ -27,7 +27,7 @@ var SidebarMenuEffects = ( function( ) {
     var bodyClickFn = function(evt) {
       if( !hasParentClass( evt.target, 'menu' ) ) {
         resetMenu();
-        document.removeEventListener( eventtype, bodyClickFn );
+        document.removeEventListener( evt.type, bodyClickFn );
       }
     }
     var button = document.getElementById( 'drawer-switch' );
@@ -54,3 +54,38 @@ var SidebarMenuEffects = ( function( ) {
   init( );
   
 })( );
+
+$( function( ) {
+
+  /* Parallax Movement */
+  var $content = $( '.content' );
+
+  $( '.slide' ).each( function ( position ) {
+    var $slide = $( this );
+    // console.debug( $slide.position( ).top );
+    // $slide.data.postop = position*1000;
+    $slide.data( 'postop', position*1000 );
+
+    $content.scroll( function( ) {
+      console.debug( );
+      var yPos = -( $content.scrollTop( ) / $slide.data( 'speed' ) ) + 0.125*position*1000 - 50;
+      var coords = '50% ' + yPos + 'px';
+      $slide.css({ 'background-position': coords });
+    });
+  });
+
+  /* Navigation Bar  */
+  $( '#navbar menu li' ).bind( 'click', function( e ) {
+      e.preventDefault( );
+      e.stopPropagation( );
+
+      var $anchor = $( this );
+      console.debug( $( $anchor.data( 'target' ) ).offset( ) );
+
+      $content.animate({
+        scrollTop: $( $anchor.data( 'target' ) ).data( 'postop' )
+      }, 2000,'easeInOutExpo');
+  });
+});
+
+  
