49290e0aa1151d6a6c483cca414a48b6c86b308e
Patrick <pevargas90@gmail.com>
2014-02-24 12:44:47 -0700

    Renamed and Added Photos

---
+++ flat/assets/repo.js
@@ -0,0 +1,264 @@
+// Patrick E. Vargas 
+// www.vargascorpus.com
+
+var repo = {
+  // Design Page
+  "button-glbtq-rc-closets.png" :
+  { 
+    date: "2011-04",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Closets are for Clothes\"."
+  },
+  "button-glbtq-rc-diversity.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Celebrate Diversity!\"."
+  },
+  "button-glbtq-rc-fabulous.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Gay by Birth, Fabulous by Choice\"."
+  },
+  "button-glbtq-rc-gay-day.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Have a Gay Day!\" with a smiley face"
+  },
+  "button-glbtq-rc-hate.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Hate Free Zone\""
+  },
+  "button-glbtq-rc-homophobia.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Homophobia: That's SO Last Millennium!\""
+    
+  },
+  "button-glbtq-rc-ralhpie-black.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button depicts CU's mascot, Ralphie, with rainbow colors"
+    
+  },
+  "button-glbtq-rc-ralhpie-white.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button depicts CU's mascot, Ralphie, with rainbow colors"
+    
+  },
+  "button-glbtq-rc-tolerate.png":
+  {
+    date: "2010-05",
+    description: "Designed buttons for the Gay, Lesbian, Bisexual, Transgender, Queer Resource Center to improve visibility. Button reads, \"Don't Tolerate Intolerance\""
+    
+  },
+  "buttons-sage-gear-fabulous-world.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts a gear in the background and reads, \"Engineering a more fabulous world!\""
+  },
+  "buttons-sage-gear-so-gay.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts a gear in the background and reads, \"Engineering is SO gay!\""
+  },
+  "buttons-sage-logo-spelt-out.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE logo with the initialism spelled out below it."
+  },
+  "buttons-sage-logo-stacked.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE logo with the initialism spelled out next to it"
+  },
+  "buttons-sage-logo.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE logo"
+  },
+  "buttons-sage-ralphie-fabulous-world.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE Ralphie logo and reads, \"Engineering a more fabulous world!\""
+  },
+  "buttons-sage-ralphie-so-gay.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE Ralphie logo and reads, \"Engineering is SO gay!\""
+  },
+  "buttons-sage-ralphie-tramp-stamp.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE Ralphie logo with the SAGE logo"
+  },
+  "buttons-sage-ralphie.png":
+  {
+    date: "2012-02",
+    description: "Designed buttons for the Student Alliance of GLBT Engineers. Button depicts the SAGE Ralphie logo"
+  },
+  "flyer-gsa-drag-show-jungle.png":
+  {
+    date: "2010-08",
+    description: "Designed flyers for the Gay-Straight Alliance. This flyer was used for the annual Drag Show, which had a jungle theme"
+  },
+  "flyer-gsa-drag-show-retro.png":
+  {
+    date: "2010-08",
+    description: "Designed flyers for the Gay-Straight Alliance. This flyer was an alternate design for the annual Drag Show"
+  },
+  "flyer-gsa-queer-sex-ed-01.png":
+  {
+    date: "2010-09",
+    description: "Designed flyers for the Gay-Straight Alliance. This alternate flyer was for the annual Queer Sex Ed event."
+  },
+  "flyer-gsa-queer-sex-ed-02.png":
+  {
+    date: "2010-09",
+    description: "Designed flyers for the Gay-Straight Alliance. This alternate flyer was for the annual Queer Sex Ed event."
+  },
+  "flyer-gsa-queer-sex-ed-03.png":
+  {
+    date: "2010-09",
+    description: "Designed flyers for the Gay-Straight Alliance. This alternate flyer was for the annual Queer Sex Ed event."
+  },
+  "flyer-gsa-queer-sex-ed-04.png":
+  {
+    date: "2010-09",
+    description: "Designed flyers for the Gay-Straight Alliance. This flyer was used for the annual Queer Sex Ed event."
+  },
+  "flyer-knitting-man.png":
+  {
+    date: "2012-08",
+    description: "Designed flyers for Knitting Man: A Gay Men's Knitting Group. This promotional handbill is used to gain new members"
+  },
+  "flyer-sage-general-meeting.png":
+  {
+    date: "2011-11",
+    description: "Designed flyers for the Student Alliance of GLBT Engineers. This promotional handbill was used to gain new members"
+  },
+  "flyer-sage-interview-workshop.png":
+  {
+    date: "2012-03",
+    description: "Designed flyers for the Student Alliance of GLBT Engineers. This flyer was used to for an Interview Workshop for GLBTQ and Ally Students"
+  },
+  "flyer-sage-mass-meeting.png":
+  {
+    date: "2012-02",
+    description: "Designed flyers for the Student Alliance of GLBT Engineers. This flyer was for our introductory meeting for the semester"
+  },
+  "flyer-sage-resume-workshop.png":
+  {
+    date: "2011-11",
+    description: "Designed flyers for the Student Alliance of GLBT Engineers. This flyer was for a Resume Writing for GLBTQ and Ally Students workshop, which was a collaborative effort between SAGE, the CU GLBTQ Business Leaders Alliance and the University of Colorado Career Services"
+  },
+  "logo-knitting-man.png":
+  {
+    date: "2012-08",
+    description: "Designed the logo for Knitting Man: A Gay Men's Knitting Group"
+  },
+  "postcard-mhfb-freedom-swing-formal-affair.png":
+  {
+    date: "2014-01",
+    description: "Designed promotional materials for Freedom Swing, the Jazz Band arm of the Mile High Freedom Band. This postcard was used to send to community members to promote the Formal Affair Swing Jazz concert"
+  },
+  "poster-quote-anonymous.png":
+  {
+    date: "2013-07",
+    description: "The poster was a side project while designing my business cards. Poster reads, \"Everything will be okay in the end. If it's not okay, it's not the end\", by Anonymous"
+  },
+  "poster-quote-zigler.png":
+  {
+    date: "2013-07",
+    description: "The poster was a side project while designing my business cards. Poster reads, \"The chief cause of failure and unhappiness is trading what you want most for what you want right now\" by Zig Ziglar"
+  },
+  "school-macbeth-cover.png":
+  {
+    date: "2012-07",
+    description: "This program was used for my class, Shakespeare for Non-majors. It is a program for play, \"The Tragedy of Macbeth\""
+  },
+  "school-macbeth-inside.png":
+  {
+    date: "2012-07",
+    description: "This program was used for my class, Shakespeare for Non-majors. It is a program for play, \"The Tragedy of Macbeth\""
+  },
+
+  // Code Page
+  "gamlet-design-frogger.png":
+  {
+    date: "",
+    link: "",
+    description: ""
+  },
+  "gamlet-design-funky.png":
+  {
+    date: "",
+    description: ""
+  },
+  "gamlet-design-robot.png":
+  {
+    date: "",
+    description: ""
+  },
+  "gamlet-design-sokoban.png":
+  {
+    date: "",
+    description: ""
+  },
+  "grails-geostan-florida.png":
+  {
+    date: "",
+    description: ""
+  },
+  "grails-geostan-hampton.png":
+  {
+    date: "",
+    description: ""
+  },
+  "grails-geostan-home.png":
+  {
+    date: "",
+    description: ""
+  },
+  "grails-geostan-vegas.png":
+  {
+    date: "",
+    description: ""
+  },
+  "grails-geostan-yelp.png":
+  {
+    date: "",
+    description: ""
+  },
+  "lamp-jamwalkr-01.png":
+  {
+    date: "",
+    description: ""
+  },
+  "lamp-jamwalkr-02.png":
+  {
+    date: "",
+    description: ""
+  },
+  "opengl-graphics-animation.png":
+  {
+    date: "",
+    description: ""
+  },
+  "website-cu-bookstore.png":
+  {
+    date: "",
+    description: ""
+  },
+  "website-sage.png":
+  {
+    date: "",
+    description: ""
+  },
+  "website-version01.png":
+  {
+    date: "",
+    description: ""
+  }
+
+};
