3b1e92811d26a71337714b2452b1a7598fb34787
Patrick <pevargas90@gmail.com>
2014-02-28 18:20:51 -0700

    Finished off website

--- flat/assets/flat.js
+++ flat/assets/flat.js
@@ -10,14 +10,19 @@
 //
 
 var vargas = {
+  // Data object for the descriptions of pictures and projects
+  data: [],
+
+  // Used in SidebarMenuEffects
   hasParentClass: function( e, classname ) {
     if ( e === document ) return false;
     if ( e.classList.contains( classname ) ) {
       return true;
     }
     return e.parentNode && vargas.hasParentClass( e.parentNode, classname ); 
-  },
+  }, // vargas.hasparentclass
 
+  // From Codrops
   SidebarMenuEffects: function( ) {
     var container = document.getElementById( 'container' );
     var resetMenu = function() { 
@@ -47,8 +52,9 @@ var vargas = {
       // Listen for an anti-click
       document.addEventListener( 'click', bodyClickFn );
     });
-  },
+  }, // vargas.sidebarmenueffects
 
+  // Function used for parallax scrolling
   parallax: function ( index ) {
     var $content = $( '.content' );
 
@@ -80,8 +86,9 @@ var vargas = {
         }
       });
     });    
-  },
+  }, // vargas.parallax
 
+  // a couple functions used for the homepage
   index: function( ) {
     var $content = $( '.content' );
     var scrollTop = $content.scrollTop( );
@@ -96,9 +103,24 @@ var vargas = {
         scrollTop: $( $anchor.data( 'target' ) ).data( 'postop' )
       }, 2000,'easeInOutExpo');
     });
-  },
 
+    // Bind reCAPTCHA
+    $( '#contact' ).click( function( ) {
+      window.open(
+        'http://www.google.com/recaptcha/mailhide/d?k\07501YyRI2wKXNvHUh53YqdwuOg\75\75\46c\75M0JEvRUGKMLUofZkk_8qKn91PUgGqk7kn1dZj3KAUb8\075',
+        '',
+        'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'
+      );
+      return false;
+    });
+  }, // vargas.index
+
+  // Function used for the lightbox effect
   lightbox: function( ) {
+    document.createElement( "caption" );
+    document.createElement( "time" );
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
     /* Light Box BEGIN */
     $( '#lightbox' ).click( function ( ) {
       $(this).fadeOut('slow');
@@ -113,26 +135,107 @@ var vargas = {
         .load( function( ) {
           width  = this.width;   // Note: $(this).width() will not
           height = this.height;  // work for in memory images.
-          if ( $( window ).height( ) < height ) {
+          var offset = 0;
+
+          if ( vargas.description( this ) ) { offset = 400; }
+
+          // if ( $( window ).height( ) < height ) {
             // width / height = x / winheight
             var ratio = width / height;
             height    = Math.floor( $( window ).height( ) * 0.9 );
             width     = Math.floor( ratio * height );
-          }
+          // }
 
-          if ( $( window ).width( ) < width ) {
+          if ( $( window ).width( ) < width + offset ) {
             var ratio  = height / width;
-            width      = Math.floor( $( window ).width( ) * 0.9 );
+            width      = Math.floor( $( window ).width( ) * 0.9 ) - offset;
             height     = Math.floor( ratio * width );
           }
-
         });
       $( this ).click( function ( ) {
-        $( '#boxcontent' ).html( '<img src="' + src + '" height="' + height + '" width="' + width + '">' ).height( height ).width( width );
+        // Get Description
+        var item = vargas.description( this );
+        var string = '<figure><img src="' + src + '" height="' + height + '" width="' + width + '"></figure>';
+        var offset = 0;
+        if ( item ) {
+          string += '<figcaption>';
+
+          offset = 300;
+          // Title
+          if ( item.title ) {
+            string += '<h2>';
+            if ( item.link ) {
+              string += '<a href="' + item.link + '" target="_blank" rel="nofollow">'
+            }
+            string += item.title;
+            if ( item.link ) {
+              string += '</a>'
+            }
+            string += '</h2>';
+          }
+
+          // Date
+          if ( item.date ) {
+            var date  = item.date.split( "-" );
+            var month = monthNames[ parseInt( date[ 1 ] ) - 1 ];
+            var year  = date[0];
+            string += '<time datetime="' + item.date + '">' + month + ' ' + year + '</time>';
+          }
+
+          // Description
+          if ( item.text ) {
+            string += '<p>' + item.text + '</p>';
+          }
+
+          string += '</figcaption>';
+        }
+
+        $( '#boxcontent' ).html( string ).height( height ).width( width + offset );
 
         // Enter lightbox
         $( '#lightbox' ).fadeIn( 'slow' );
       });
     });
-  }
+  }, // vargas.lightbox
+
+  key: function( ) {
+    $( window ).keyup( function ( e ) {
+      var images  = $( "article img" );
+      var current = $( "#boxcontent img" )[0];
+      var index   = 0;
+
+      if ( current ) {
+
+        // Find the index of the currently displayed image on the pages
+        for ( var i = 0; i < images.length; ++i ) {
+          if ( images[i].src == current.src ) {
+            index = i;
+            console.debug( i );
+            break;
+          }
+        }
+        
+        // Left Arrow
+        if ( e.which == 37 ) index--;
+        // Right Arrow
+        if ( e.which == 39 ) index++;
+
+        index = ( index + images.length ) % images.length;
+        images[ index ].click( );
+      }
+    });
+  },
+
+  // Function to get the description of a project
+  description: function ( e ) {
+    var arr  = e.src.split( "/" );
+    var item = arr[arr.length - 1];
+
+    if ( repo ) {
+      return repo[item];
+    }
+
+    return null;
+  } // vargas.description
+
 };
